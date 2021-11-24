4e6c3e39e6ab268a535d1fed1b69c17d221cc766
richsmithrr <richard@datatheorem.io>
2021-11-24 13:11:47 -0500

    Add a new feature

--- app/src/main/java/com/datatheorem/dt_sast_demo/App.java
+++ app/src/main/java/com/datatheorem/dt_sast_demo/App.java
@@ -3,12 +3,38 @@
  */
 package com.datatheorem.dt_sast_demo;
 
+import java.io.*;
+import java.util.zip.*;
+
 public class App {
     public String getGreeting() {
         return "Hello World!";
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println(new App().getGreeting());
+
+		// Let's write some insecure code...
+		//
+		// 1. Download a .zip file over the network
+		ZipInputStream zis = new ZipInputStream(new FileInputStream("poc.zip"));
+		// 2. Iterate each file listed in the .zip archive
+		ZipEntry ent = zis.getNextEntry();
+		while (ent != null) {
+			// 3. Extract the zip entry to the local filesystem
+			String fileName = ent.getName(); 
+			FileOutputStream fos = new FileOutputStream(fileName);
+			byte[] bytes = new byte[128];
+			int size = zis.read(bytes);
+			while (size != -1) {
+				fos.write(bytes, 0, size);
+				size = zis.read(bytes);
+			}
+			// 4. Iterate to the next file listed in the .zip archive, if any
+			ent = zis.getNextEntry();
+		}
+		// 5. Clean up
+		zis.closeEntry();
+		zis.close();
     }
 }
